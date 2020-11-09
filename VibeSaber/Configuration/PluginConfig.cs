eee16ea43c8299fa0fbe9e841ac9172472b29eeb
Tyler Schrock <tschrock123@gmail.com>
2020-11-09 01:08:23 -0500

    Add enum formatter

--- VibeSaber/Configuration/PluginConfig.cs
+++ VibeSaber/Configuration/PluginConfig.cs
@@ -4,7 +4,12 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 
 using IPA.Config.Stores;
+using IPA.Config.Stores.Attributes;
+using IPA.Config.Stores.Converters;
+
 using BeatSaberMarkupLanguage.Attributes;
+using System.Reflection;
+using System.ComponentModel;
 
 [assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
 namespace VibeSaber.Configuration
@@ -32,6 +37,7 @@ namespace VibeSaber.Configuration
         private List<object> PulseModeOptions = Enum.GetValues(typeof(Configuration.PulseMode)).Cast<object>().ToList();
 
         [UIValue(nameof(PulseMode))]
+        [UseConverter(typeof(EnumConverter<PulseMode>))]
         public virtual PulseMode PulseMode { get; set; } = DefaultSettings.PulseMode;
 
         [UIValue(nameof(PulseLength))]
@@ -41,6 +47,7 @@ namespace VibeSaber.Configuration
         private List<object> StrengthModeOptions = Enum.GetValues(typeof(Configuration.StrengthMode)).Cast<object>().ToList();
 
         [UIValue(nameof(StrengthMode))]
+        [UseConverter(typeof(EnumConverter<StrengthMode>))]
         public virtual StrengthMode StrengthMode { get; set; } = DefaultSettings.StrengthMode;
 
         [UIValue(nameof(MaximumStrength))]
@@ -48,5 +55,28 @@ namespace VibeSaber.Configuration
 
         [UIValue(nameof(MinimumStrength))]
         public virtual int MinimumStrength { get; set; } = DefaultSettings.MinimumStrength;
+
+        [UIAction(nameof(FormatEnum))]
+        private string FormatEnum(object value)
+        {
+            string enumName = value.ToString();
+            Type enumType = value.GetType();
+            MemberInfo[] memberInfos = enumType.GetMember(enumName);
+            if (memberInfos.Any())
+            {
+                MemberInfo memberInfo = memberInfos.First();
+                NameAttribute? nameAttribute = memberInfo.GetCustomAttribute<NameAttribute>();
+                if (nameAttribute != null)
+                {
+                    return nameAttribute.DisplayName;
+                }
+                DescriptionAttribute? descriptionAttribute = memberInfo.GetCustomAttribute<DescriptionAttribute>();
+                if (descriptionAttribute != null)
+                {
+                    return descriptionAttribute.Description;
+                }
+            }
+            return enumName;
+        }
     }
 }
